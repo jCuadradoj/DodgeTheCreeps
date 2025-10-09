b37eb7f7273737f9c6eef0b059da7950e51d84d9
jCuadradoj <jessica.cuadradoj@educem.net>
2025-10-09 20:52:34 +0200

    Escena principal

--- Player.gd
+++ Player.gd
@@ -3,19 +3,9 @@ signal hit
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg). 
 var screen_size # Mida de la finestra de joc. 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide() 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _process(delta): 
 	var velocity = Vector2.ZERO # Vector de moviment del jugador. 
